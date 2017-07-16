d688d0ff251e22344478efe1e0c0c10f01d7c31f
Daniel Endres <danielendres@gmail.com>
2017-07-15 18:28:27 -0700

    integration works

---
+++ app/assets/javascripts/jquery.js
@@ -0,0 +1,88 @@
+const $microphone = $('.active-mic')
+const $firstWave = $('.soundwavesSVG1')
+const $secondWave = $('.second-wave')
+const $thirdWave = $('.third-wave')
+
+//$secondWave.hide()
+//$secondWave.show()
+
+
+function colorChange() {
+$($firstWave).animate([
+	{fill: 'cyan'},
+	{fill: '#e6e6e6'}
+
+	],
+	{
+	duration: 1000
+})
+}
+
+let firstWave = document.querySelector('.firstWave')
+
+let animateColor = firstWave.animate([
+	{fill: 'cyan'},
+	{fill: '#e6e6e6'}
+],
+
+{
+	duration: 1000
+})
+
+
+function animateCloud() {
+       $($firstWave).animate([
+		                     { fill: 'cyan' },
+							 {fill: '#e6e6e6'}
+							 ]
+							 { duration: 3500, easing: "ease-out" })
+                  .animate( [ { fill: '#e6e6e6' },
+							  { fill: 'cyan' }
+
+							 ], { duration: 3500, easing: "ease-out", complete: animateCloud });
+   }
+
+$(function() {
+    animateCloud();
+
+});
+
+
+
+
+//
+//let secondWaveAnimation = secondWave.animate([
+//	{fill: 'cyan'},
+//	{fill: '#e6e6e6'}
+//
+//	],
+//	{
+//	duration: 1000
+//})
+//
+//let thirdWaveAnimation = thirdWave.animate([
+//	{fill: 'cyan'},
+//	{fill: '#e6e6e6'}
+//
+//	],
+//	{
+//	duration: 1000
+//})
+//
+//let recursiveWave = (firstWave,secondWave,thirdWave) => {
+//	firstWave.play()
+//	secondWave.play()
+//	thirdWave.play()
+//
+//	recursiveWave(firstWaveAnimation,secondWaveAnimation,thirdWaveAnimation)
+//}
+
+
+
+
+//$(window).resize(function() {
+//    clearTimeout(window.resizedFinished)
+//    window.resizedFinished = setTimeout(function(){
+//      console.log($(window).width())
+//    }, 250);
+//  });
